133db47f70264fe133d0f911ef831df8223c0f8a
Randolph Pe Benito <randolph@xy-randolph.xyber.ph>
2016-08-08 15:35:26 +0800

    Fixed celery config.

--- webapp/webapp/celery.py
+++ webapp/webapp/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp.settings')
 app = Celery('webapp')
 
 # Using a string here means the worker will not have to
