c46e9e34f7c5a8ce046c01309dcd6874a0a85ade
Randolph Pe Benito <randolph@xy-randolph.xyber.ph>
2016-08-09 14:27:26 +0800

    Added url for home page.

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from feedback.views import FeedbackView
+from photos.views import PhotoView
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^feedback/$', FeedbackView.as_view(), name="feedback"),
+    url(r'^$', PhotoView.as_view(), name="home"),
+
 ]
